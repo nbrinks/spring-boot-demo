5318e078d3e5d176d60529a44420aae7f3e1f0c6
Nathan Brinks <nathanb@spindance.com>
2019-05-09 15:47:30 -0500

    update

--- src/test/java/com/example/demo/FooTest.java
+++ src/test/java/com/example/demo/FooTest.java
@@ -10,11 +10,23 @@ public class FooTest {
   public void getBar() {
     Foo foo = new Foo("abc", "def");
     assertThat(foo.getBar()).isEqualTo("abc");
+    foo.setBar("xyz");
+    assertThat(foo.getBar()).isNotEqualTo("abc");
   }
 
   @Test
   public void getBaz() {
     Foo foo = new Foo("abc", "def");
     assertThat(foo.getBaz()).isEqualTo("def");
+    foo.setBaz("xyz");
+    assertThat(foo.getBaz()).isNotEqualTo("def");
+  }
+
+  @Test
+  public void isAwesome() {
+    Foo foo = new Foo("abc", "def");
+    assertThat(foo.isAwesome()).isFalse();
+    foo.setBaz("abc");
+    assertThat(foo.isAwesome()).isTrue();
   }
 }
